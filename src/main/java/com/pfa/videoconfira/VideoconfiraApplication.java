580728672272003fe6dbf523070996965624d96f
AbderahmenDaai <abderahmendaai@gmail.com>
2021-02-25 11:02:06 +0100

    first+ commit

---
+++ src/main/java/com/pfa/videoconfira/VideoconfiraApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.videoconfira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoconfiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoconfiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfa/videoconfira/VideoconfiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.videoconfira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoconfiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
